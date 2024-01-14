62b5a0149ee3611b5acda0b05c4aa8ee3fcf1378
Bartosz Lipiński <bartosz.s.lipinski@gmail.com>
2024-01-14 19:43:21 +0100

    refactor: change PokemonCard into list item

---
+++ src/components/PokemonItem.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <li class="flex items-center justify-between rounded-lg border px-4 py-2 shadow-sm">
+    <h3 class="font-medium">Bulbasaur</h3>
+    <div class="flex items-center gap-x-4">
+      <p class="text-sm text-green-500">In team</p>
+      <button
+        type="button"
+        class="rounded bg-red-500 px-2 py-1 text-sm font-semibold text-white shadow-sm transition-colors hover:bg-red-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      >
+        Details
+      </button>
+    </div>
+  </li>
+</template>
--- src/views/PokedexView.vue
+++ src/views/PokedexView.vue
@@ -1,18 +1,18 @@
 <template>
   <main class="mx-auto flex w-full max-w-screen-md grow flex-col overflow-y-auto p-4">
     <search-input />
-    <div class="grid gap-4 sm:grid-cols-2">
-      <pokemon-card />
-      <pokemon-card />
-    </div>
+    <ul class="space-y-2">
+      <pokemon-item />
+      <pokemon-item />
+    </ul>
   </main>
   <pokedex-pagination />
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import SearchInput from '@/components/SearchInput.vue';
-import PokemonCard from '@/components/PokemonCard.vue';
+import PokemonItem from '@/components/PokemonItem.vue';
 import PokedexPagination from '@/components/PokedexPagination.vue';
 
 onMounted(() => {
