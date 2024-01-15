a03c75a70152b1a8010a034a7fd58a3f32317bbc
Bartosz Lipiński <bartosz.s.lipinski@gmail.com>
2024-01-15 21:17:30 +0100

    feat: fetch pokemons

--- src/components/PokemonItem.vue
+++ src/components/PokemonItem.vue
@@ -1,10 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const props = defineProps(['pokemon']);
+</script>
 
 <template>
   <li class="flex items-center justify-between rounded-lg border px-4 py-2 shadow-sm">
-    <h3 class="font-medium">Bulbasaur</h3>
+    <h3 v-if="pokemon" class="font-medium capitalize">{{ props.pokemon }}</h3>
+    <h3 v-else class="h-6 w-32 animate-pulse rounded bg-neutral-300"></h3>
     <div class="flex items-center gap-x-4">
-      <p class="text-sm text-green-500">In team</p>
+      <!--   TODO: Show if pokemon is in team   -->
+      <!--   <p class="text-sm text-green-500">In team</p>    -->
       <button
         type="button"
         class="rounded bg-red-500 px-2 py-1 text-sm font-semibold text-white shadow-sm transition-colors hover:bg-red-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
--- src/views/PokedexView.vue
+++ src/views/PokedexView.vue
@@ -1,9 +1,19 @@
 <template>
   <main class="mx-auto flex w-full max-w-screen-md grow flex-col overflow-y-auto p-4">
-    <search-input />
     <ul class="space-y-2">
-      <pokemon-item />
-      <pokemon-item />
+      <template v-if="isLoading">
+        <pokemon-item v-for="index in 10" :key="index" :pokemon="null" />
+      </template>
+      <template v-else>
+        <template v-if="pokemons.length === 0">
+          <li class="flex items-center justify-center rounded-lg border px-4 py-2 shadow-sm">
+            <h3 class="font-medium">No results found</h3>
+          </li>
+        </template>
+        <template v-else>
+          <pokemon-item v-for="pokemon in pokemons" :key="pokemon" :pokemon="pokemon.name" />
+        </template>
+      </template>
     </ul>
   </main>
   <pokedex-pagination />
@@ -11,11 +21,23 @@
 
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
-import SearchInput from '@/components/SearchInput.vue';
+import axios from 'axios';
+
 import PokemonItem from '@/components/PokemonItem.vue';
 import PokedexPagination from '@/components/PokedexPagination.vue';
+
+const isLoading = ref(true);
+const pokemons = ref<{ name: string; url: string }[]>([]);
+
+const fetchPokemons = async () => {
+  const { data } = await axios('https://pokeapi.co/api/v2/pokemon', { params: {} });
+  pokemons.value = data.results;
+  isLoading.value = false;
+};
 
 onMounted(() => {
   document.title = 'Pokèdex';
 });
+
+fetchPokemons();
 </script>
